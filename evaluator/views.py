28a3528313256305a0a2ba7035dd21304bcdfe94
Omar <omarh.ismail1@gmail.com>
2019-04-15 01:54:57 +0200

    Json Response Made more Clear

--- evaluator/views.py
+++ evaluator/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import Company, Company_Field
+from django.core import serializers
 
 # Create your views here.
 
@@ -15,13 +16,31 @@ def detail(request, company_id):
 @csrf_exempt
 def recommend(request):
       json_data = json.loads(request.body)
-      fields = Company_Field.objects.filter(field_name=json_data['AOI']).filter(field_minimum_score__lte=json_data['Score'])
+      fields = Company_Field.objects.filter(field_name=json_data['FOI']).filter(field_minimum_score__lte=json_data['Score'])
       companies = []
+      rec = []
+      co=0
       for field in fields:
-            companies.append(get_object_or_404(Company, id=field.company_id).company_name)
+            companies.append(get_object_or_404(Company, id=field.company_id))
+            rec.append(
+                  {
+                        'company_id' : companies[co].pk,
+                        'company_name' : companies[co].company_name,
+                        'field' : field.field_name
+                  }
+            )
+            print(companies[co].pk)
+            co+= 1
 
       # return HttpResponse(companies)
       responseData = {
-        'recommended_companies' : companies
+        'recommended_companies' : rec
       }
+      # response_data = {}
+      # try:
+      #       response_data['result'] = 'Success'
+      #       response_data['recommended_companies'] = serializers.serialize('json', companies)
+      # except:
+      #       response_data['result'] = 'Ouch!'
+      #       response_data['recommended_companies'] = 'Script has not ran correctly'
       return JsonResponse(responseData)
